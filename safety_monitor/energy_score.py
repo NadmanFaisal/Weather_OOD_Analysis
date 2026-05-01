b3e4d2a4cac5e1752ec9faea91db18ee020611dd
Nadman Faisal <nadmanfaisal@gmail.com>
2026-05-01 10:49:26 +0200

    #17 Implement simple script to calculate energy scores

--- safety_monitor/energy_score.py
+++ safety_monitor/energy_score.py
@@ -1,5 +1,13 @@
 import torch
+import os
 
+current_dir = os.path.dirname(os.path.abspath(__file__))
+
+file_path = os.path.abspath(os.path.join(
+    current_dir,
+    "../",
+    "data/intercepted_logits/Fog/easy/20260501_000617/head_final_logits_batch_0_gpu_57155.pt"
+))
 
 def compute_energy_score(logits: torch.Tensor, T: float = 1.0) -> torch.Tensor:
     """Compute the energy score for a batch of logits.
@@ -18,3 +26,10 @@ def compute_energy_score(logits: torch.Tensor, T: float = 1.0) -> torch.Tensor:
     """
     scores = -T * torch.logsumexp(logits / T, dim=1)
     return scores.detach()
+    model_state_dict = torch.load(file_path)
+    print("Model dict:", model_state_dict)
+    print("Shape of Model dict:", model_state_dict.shape)
+
+    energy_score = compute_energy_score(model_state_dict)
+    print("Energy score", energy_score)
+    print("Shape of energy score:", energy_score.shape)
