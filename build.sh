6210c3f8b98406f878c911ce7509184261f9dea0
Nadman Faisal <nadmanfaisal@gmail.com>
2026-07-28 12:11:32 +0200

    #51 Script for building artifact sif file

---
+++ build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --partition=gpu
+#SBATCH --gpus-per-node=1
+#SBATCH --time=01:00:00
+#SBATCH --account=naiss2026-4-688-gpu
+
+echo "Starting ARM64 container build on compute node..."
+rm -f bevformer_native.sif
+
+# Notice the added flag here:
+apptainer build --fakeroot --ignore-fakeroot-command bevformer_native.sif bevformer_native.def
+
+echo "Build complete!"
