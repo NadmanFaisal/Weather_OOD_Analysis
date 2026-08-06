b32d309ba279fcb8ca635de52120aed10e77457c
Nadman Faisal <nadmanfaisal@gmail.com>
2026-08-06 10:18:58 +0200

    #53 Reorganize automold scripts

--- scripts_automold/automold_corruption.py
+++ scripts_automold/automold_corruption.py
@@ -12,7 +12,7 @@ import Automold as am
 # Gets the project directory's name
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
-from constants import OUTPUT_DIR, REGION, FILES
+from constants import OUTPUT_DIR
 
 ROOT_DIR = Path(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
